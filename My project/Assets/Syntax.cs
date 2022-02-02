564e01a60c0a6532b51aea6aa4400f5adbe9addb
William Thackeray <will_thack@comcast.net>
2022-02-02 11:15:16 -0700

    fixed error

--- My project/Assets/Syntax.cs
+++ My project/Assets/Syntax.cs
@@ -15,7 +15,7 @@ public class Syntax : MonoBehaviour
 
         Debug.Log(transform.position.x);
 
-        if(transform.position.x);
+        if(transform.position.y <= 5f);
         {
             Debug.Log ("I'm about to hit the ground!");
         }
