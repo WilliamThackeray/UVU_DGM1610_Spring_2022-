9bfa0ed75a44f43ef40290d588f1ecb1c2c203fa
William Thackeray <will_thack@comcast.net>
2022-02-03 10:11:19 -0700

    Added Main Player Script

---
+++ My project/Assets/MainPlayer.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainPlayer : MonoBehaviour
+{
+    void Start()
+    {
+        Debug.Log("I am ALIVE! YAHOOOOOOO!!")
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
