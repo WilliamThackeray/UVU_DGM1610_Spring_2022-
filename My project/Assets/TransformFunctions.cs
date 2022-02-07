e8a3ed1e57c3c4ce03b629b757ea631ce9dea3ba
William Thackeray <will_thack@comcast.net>
2022-02-07 08:33:52 -0700

    Added Transform Functions

---
+++ My project/Assets/TransformFunctions.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransformFunctions : MonoBehaviour
+{
+    public float moveSpeed = 10f;
+    public float turnSpeed = 50f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.UpArrow))
+            TransformFunctions.Translate(Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.DownArrow))
+            TransformFunctions.Translate(-Vector3.forward * moveSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+            TransformFunctions.Translate(Vector3.up * -turnSpeed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.RightArrow))
+            TransformFunctions.Translate(Vector3.up * turnSpeed * Time.deltaTime);
+    }
+}
