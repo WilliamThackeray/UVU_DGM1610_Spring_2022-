2dd7aebd44595204ce2dcedf945b27a1b412ac71
William Thackeray <will_thack@comcast.net>
2022-02-09 20:08:59 -0700

    added AxisExample

---
+++ My project/Assets/AxisExample.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AxisExample : MonoBehaviour
+{
+    public float range;
+    public Text textOutput;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = ButtonInput.GetAxis("Horizontal");
+        float xPos = h * range;
+
+        transform.position = new Vector3(xPos, 2f, 0);
+        textOutput.text = "Value Returned: " + h.ToString("F2");
+    }
+}
