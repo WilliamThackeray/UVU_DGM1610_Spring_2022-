70377a3d5e88a9136a784b067e520ec34e91e9e2
William Thackeray <will_thack@comcast.net>
2022-02-16 10:40:29 -0700

    Added variables

---
+++ My project/Assets/Variables.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Variables : MonoBehaviour
+{
+    int health = 100;
+    int attackDamage = 5;
+    int accuracy = 95;
+    
+}
