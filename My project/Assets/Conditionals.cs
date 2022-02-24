322440933fc404131a4d025a1fc66dd716762b0e
William Thackeray <will_thack@comcast.net>
2022-02-24 12:02:54 -0700

    Added the Update() function with conditional to test if the enemy has died yet.

--- My project/Assets/Conditionals.cs
+++ My project/Assets/Conditionals.cs
@@ -1,48 +1,29 @@
-using System;
 using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
   
-class Conditionals {
-  
-    void Start()
+public class Conditionals : MonoBehaviour 
+{
+    public int playerHealth = 100;
+    public int enemyHealth = 100;
+
+    private void Awake() 
     {
-      Dictionary<string, int> healthItems = new Dictionary<string, int>();
-      Dictionary<string, int> attackItems = new Dictionary<string, int>();
-      Dictionary<string, int> enemies = new Dictionary<string, int>();
 
-      healthItems.Add("helmet", 10);
-      healthItems.Add("chestplate", 20);
-      healthItems.Add("pants", 15);
-      healthItems.Add("boots", 5);
+    }
+    void Update() 
+    {
+      Debug.Log("Player health: " + playerHealth);
+      Debug.Log("Enemy health: " + enemyHealth);
 
-      attackItems.Add("sword", 10);
-      attackItems.Add("knife", 1);
-      attackItems.Add("bow", 6);
-      attackItems.Add("spear", 12);
-
-      enemies.Add("golem", 200);
-      enemies.Add("rat", 5);
-      enemies.Add("zombie", 50);
-      enemies.Add("ghoul", 65);
-      
-      
-      var playerHealth = 100;
-      var enemyHealth = enemies["zombie"];
-      
-      while (playerHealth > 0 && enemyHealth > 0) {
-        // take input here
+      if (playerHealth == 0) {
+        Debug.Log("Player has been killed");
+      } else if (enemyHealth == 0) {
+        Debug.Log("Enemy has been killed");
+      }
+      if (Input.GetKeyDown(KeyCode.Space)) {
         enemyHealth -= 10;
       }
-      if (playerHealth <= 0) {
-        Debug.Log("You have died.");
-      } else if (enemyHealth <= 0) {
-        Debug.Log("Enemy has been killed.");
-      }
     }
-
-    void Update() {
-
-    }
+  
 }
-
