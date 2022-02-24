214b030bca202d3bf1ff6945e37d0f0a0c8edcc4
William Thackeray <will_thack@comcast.net>
2022-02-23 22:47:18 -0700

    Added dictionaries for HealthItems and AttackItems

---
+++ My project/Conditionals.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collection.Generics; 
+  
+class Conditionals {
+  
+    // Driver code
+    public static void Main()
+    {
+      Dictionary healthItems = new Dictionary();
+      Dictionary attackItems = new Dictionary();
+      Dictionary enemies = new Dictionary();
+
+      healthItems.add("helmet", 10);
+      healthItems.add("chestplate", 20);
+      healthItems.add("pants", 15);
+      healthItems.add("boots", 5);
+
+      attackItems.add("sword", 10);
+      attackItems.add("knife", 1);
+      attackItems.add("bow", 6);
+      attackItems.add("spear", 12);
+
+      
+
+
+
+
+      var health = 100;
+
+      
+    }
+}
+
