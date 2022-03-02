133272630016fd9735b54341647948fd25238ddb
William Thackeray <will_thack@comcast.net>
2022-03-02 09:22:22 -0700

    Added array for some looping action

--- My project/Assets/ForLoopsWithArrays.cs
+++ My project/Assets/ForLoopsWithArrays.cs
@@ -5,15 +5,32 @@ using UnityEngine;
 public class ForLoopsWithArrays : MonoBehaviour
 {
     public int pHealth = 100;
+    public string[] items = {"Steel Helmet", "sword", "Fire resistance potion", "pants", "shoes", "bread"};
+    public bool hasFireResist = false;
 
     // Start is called before the first frame update
     void Start()
     {
         Debug.Log("Welcome to the Arena");
         Debug.Log("You have been lit on fire.");
-        for (int i = 0; i < 15; i++) {
-            pHealth -= 1;
-            Debug.Log("Your Health: "+ pHealth);
+        for (int i = 0; i < items.Length; i++) {
+            if (items[i] == "Steel Helmet") {
+                pHealth += 20;
+            }
+            if (items[i] == "Fire resistance potion") {
+                hasFireResist = true;
+            }
+        }
+        if (hasFireResist == false) {
+            for (int i = 0; i < 15; i++) {
+                pHealth -= 1;
+                Debug.Log("Your Health: "+ pHealth);
+            }
+        } else {
+            for (int i = 0; i < 7; i++) {
+                pHealth -= 1;
+                Debug.Log("Your Health: "+ pHealth);
+            }
         }
     }
 
