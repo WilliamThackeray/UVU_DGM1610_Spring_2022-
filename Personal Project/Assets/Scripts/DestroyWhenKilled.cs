567b4d34235a2e79766742a33b4338bf4e8b16dc
William Thackeray <will_thack@comcast.net>
2022-04-07 22:43:58 -0600

    Added enemy damage to player and player death

--- Personal Project/Assets/Scripts/DestroyWhenKilled.cs
+++ Personal Project/Assets/Scripts/DestroyWhenKilled.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class DestroyWhenKilled : MonoBehaviour
 {
+    private PlayerStats playerStatsScript;  //Gets base player stats.
     public int health = 100;
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerStatsScript = GameObject.Find("Player").GetComponent<PlayerStats>();
     }
 
     // Update is called once per frame
@@ -26,6 +27,13 @@ public class DestroyWhenKilled : MonoBehaviour
         {
             Destroy(gameObject);
         } 
+        if (other.gameObject.tag == "Player") {
+            playerStatsScript.currentHealth -= 5;
+        }
+        if (playerStatsScript.currentHealth <= 0)
+        {
+            Destroy(other.gameObject);
+        }
         
     }
 }
