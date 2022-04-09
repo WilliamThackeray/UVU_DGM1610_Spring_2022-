50207e4f835ea4b01a4c512bee4b1feb43de1afa
William Thackeray <will_thack@comcast.net>
2022-04-08 23:07:56 -0600

    Finished Voyager attack cooldown

--- Personal Project/Assets/Scripts/Voyager/VoyagerAttack.cs
+++ Personal Project/Assets/Scripts/Voyager/VoyagerAttack.cs
@@ -7,6 +7,9 @@ public class VoyagerAttack : MonoBehaviour
     public GameObject daggerPrefab1; 
     private float timer;
     private float shotTime;
+    public float attackSpeed = 1.0f; // cooldown for the attack
+    public bool cooldown = true; // has the attack cooled down?
+
     Quaternion posRotation1 = Quaternion.Euler(0, 15, 0);
     Quaternion negRotation1 = Quaternion.Euler(0, -15, 0);
     Quaternion posRotation2 = Quaternion.Euler(0, 30, 0);
@@ -21,15 +24,21 @@ public class VoyagerAttack : MonoBehaviour
     void Update()
     {
         // Primary Attack 
-        if (Input.GetKeyDown(KeyCode.M)) {
-            shotTime = Time.deltaTime;
+        if (Input.GetKey(KeyCode.M) && cooldown) {
             // instantiate dagger prefabs here
             Instantiate(daggerPrefab1, transform.position, daggerPrefab1.transform.rotation * transform.rotation);
             Instantiate(daggerPrefab1, transform.position, daggerPrefab1.transform.rotation * transform.rotation * posRotation1);
             Instantiate(daggerPrefab1, transform.position, daggerPrefab1.transform.rotation * transform.rotation * posRotation2);
             Instantiate(daggerPrefab1, transform.position, daggerPrefab1.transform.rotation * transform.rotation * negRotation1);
             Instantiate(daggerPrefab1, transform.position, daggerPrefab1.transform.rotation * transform.rotation * negRotation2);
+            // Make the cooldown happen
+            cooldown = false;
+            StartCoroutine(AttackCoolDown());
         }
 
+    }
+    IEnumerator AttackCoolDown () {
+        yield return new WaitForSeconds(attackSpeed);
+        cooldown = true;
     }
 }
